cb0b7de03c60e74e7d5979274df0128dc5b5d8f2
jvc5546 <jvc5546@gmail.com>
2019-05-28 16:19:42 -0500

    Rendering other-items Module from Jonathan, port 3003

--- client/index.jsx
+++ client/index.jsx
@@ -1,20 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import axios from 'axios';
 
-class Betsy extends React.Component {
-  constructor(props){
-    super(props);
-  }
 
-  render(){
-      return (
-      <div>
-        APPLICATION!!!!
-        <div id='trevor'></div>
-        <div id='jonathan'></div>
-      </div>
-      );
-  }
-}
+// class Betsy extends React.Component {
+//   constructor(props){
+//     super(props);
+//   }
 
-ReactDOM.render(<Betsy />, document.getElementById('app'));
+//   componentDidMount(){
+//     console.log('Mounted!');
+//     axios.get('http://localhost:3003/')
+//       .then((res) => {
+//         console.log('We are here');
+//         console.log(res.data.getElementById('jjc-app'));
+//       })
+//       .catch((err) => {
+//         console.log('Houston, we got an error', err);
+//       });
+//   }
+
+//   render(){
+//       return (
+//       <div>
+//         APPLICATION!!!!
+//         <div id='trevor'></div>
+//         <div id='jonathan'></div>
+//       </div>
+//       );
+//   }
+// }
+
+// ReactDOM.render(<Betsy />, document.getElementById('app'));
+
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
-app.use(morgan('combined'));
+app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.json());
 
