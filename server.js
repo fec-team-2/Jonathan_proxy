d0174293a382b8c27e58f93572111b98eb5fd9e5
jvc5546 <jvc5546@gmail.com>
2019-06-01 16:53:04 -0500

    First commit for deploying proxy server in AWS

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
